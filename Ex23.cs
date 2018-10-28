db3492bcefacbe9c4fbf738b6047a04a6582ec5a
golovkinV <vladimir.golovkin16@yandex.ru>
2018-10-29 01:55:49 +0500

    Ex23 30.10.2018

---
+++ Ex23.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ex23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("X = ");
+            string a = Console.ReadLine();
+            int x = int.Parse(a);
+
+            int[] numbers = CreateArray();
+            PrintElements(numbers,x);
+
+            Console.ReadLine();
+        }
+
+        static int[] CreateArray()
+        {
+            int[] array = new int[51];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = i;
+            return array;
+        }
+
+        static void PrintElements(int[] numbers, int x)
+        {
+            int firstIndex = 0;
+            int lastIndex = numbers.Length - 1;
+
+            while (firstIndex != lastIndex)
+            {
+                int sum = numbers[firstIndex] + numbers[lastIndex];
+                if (sum < x)
+                    firstIndex++;
+                else if (sum > x)
+                    lastIndex--;
+                else
+                {
+                    Console.Write("{0} + {1}", numbers[firstIndex], numbers[lastIndex]);
+                    break;
+                }
+            }
+            if (firstIndex == lastIndex)
+                Console.Write("Элементы не найдены");
+        }
+    }
+}
